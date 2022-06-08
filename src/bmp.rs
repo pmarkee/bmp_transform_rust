36dbe647a6be606a202e65ca9dc4ea7f20c64f0e
Peter Marki <marpeter00@gmail.com>
2022-06-08 22:28:24 +0200

    Some re-structuring and refactoring

--- src/bmp.rs
+++ src/bmp.rs
@@ -1,16 +1,19 @@
 use byteorder::ByteOrder;
 use byteorder::LittleEndian;
+use std::fs::File;
+use std::io::Error;
+use std::io::Read;
+use std::io::Seek;
+use std::io::SeekFrom;
 use std::iter::Iterator;
 
 // TODO rethink what should be public and private here
-#[derive(Debug)]
 pub struct Pixel {
     pub red: u8,
     pub green: u8,
     pub blue: u8,
 }
 
-#[derive(Debug)]
 pub struct BitmapFileHeader {
     pub signature: u16,
     pub other_bytes: u64,
@@ -23,17 +26,35 @@ pub struct BitmapDibHeader {
     pub height: u32,
 }
 
-#[derive(Debug)]
 pub struct Bitmap {
     pub width: u32,
     pub height: u32,
     pub pixels: Vec<Pixel>,
 }
 
-#[derive(Debug)]
 pub struct BmpFile {
     pub bitmap_file_header: BitmapFileHeader,
+    pub bitmap_dib_header: BitmapDibHeader,
     pub bitmap: Bitmap,
+}
+
+impl std::fmt::Debug for BitmapFileHeader {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::result::Result<(), std::fmt::Error> {
+        write!(
+            f,
+            "BitmapFileHeader {{ signature: {:#x}, offset: {:#x} }}",
+            self.signature, self.offset
+        )
+    }
+}
+
+impl std::fmt::Debug for BmpFile {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::result::Result<(), std::fmt::Error> {
+        f.debug_struct("BmpFile")
+            .field("bitmap_file_header", &self.bitmap_file_header)
+            .field("bitmap_dib_header", &self.bitmap_dib_header)
+            .finish()
+    }
 }
 
 pub fn read_bitmap_file_header(buffer: &[u8; 14]) -> BitmapFileHeader {
@@ -75,4 +96,42 @@ pub fn read_bitmap(buffer: &Vec<u8>, dib_header: &BitmapDibHeader) -> Bitmap {
         height: dib_header.height,
         pixels,
     }
+}
+
+pub fn read_bmp_file(path: &str) -> Result<BmpFile, Error> {
+    let mut file = File::open(&path).unwrap();
+
+    let bitmap_file_header = read_bitmap_file_header(&read_header_data(&mut file)?);
+    let bitmap_dib_header = read_dib_header(&read_dib_header_data(&mut file)?);
+    let bitmap = read_bitmap(
+        &read_bitmap_data(&mut file, &bitmap_file_header)?,
+        &bitmap_dib_header,
+    );
+
+    Ok(BmpFile {
+        bitmap_file_header,
+        bitmap_dib_header,
+        bitmap,
+    })
+}
+
+fn read_header_data(file: &mut File) -> Result<[u8; 14], Error> {
+    let mut buffer = [0; 14];
+    file.by_ref().take(14).read(&mut buffer)?;
+    Ok(buffer)
+}
+
+fn read_dib_header_data(file: &mut File) -> Result<[u8; 8], Error> {
+    let mut buffer = [0; 8];
+    file.seek(SeekFrom::Start(0x12))?;
+    file.by_ref().take(8).read(&mut buffer)?;
+    Ok(buffer)
+}
+
+fn read_bitmap_data(file: &mut File, header: &BitmapFileHeader) -> Result<Vec<u8>, Error> {
+    file.seek(SeekFrom::Start(header.offset as u64))?;
+    let mut buffer: Vec<u8> = Vec::new();
+    let bytes_read = file.read_to_end(&mut buffer)?;
+    println!("Read {:?} bytes", bytes_read);
+    Ok(buffer)
 }
--- src/main.rs
+++ src/main.rs
@@ -1,45 +1,8 @@
-use std::fs::File;
-use std::io::Error;
-use std::io::Read;
-use std::io::Seek;
-use std::io::SeekFrom;
-
 pub mod bmp;
 
 fn main() {
     let img_path = "/home/pmarkee/learning/rust/bmp_transform_rust/resources/bliss.bmp";
-    // TODO handle errors instead of unwrap
-    let mut file = File::open(&img_path).unwrap();
 
-    let header_data = read_header_data(&mut file).unwrap();
-    let header = bmp::read_bitmap_file_header(&header_data);
-    println!("{:?}", header);
-
-    let dib_header_data = read_dib_header_data(&mut file).unwrap();
-    let dib_header = bmp::read_dib_header(&dib_header_data);
-    println!("{:?}", dib_header);
-
-    let bitmap_data = read_bitmap_data(&mut file, &header).unwrap();
-    let bitmap = bmp::read_bitmap(&bitmap_data, &dib_header);
-}
-
-fn read_header_data(file: &mut File) -> Result<[u8; 14], Error> {
-    let mut buffer = [0; 14];
-    file.by_ref().take(14).read(&mut buffer)?;
-    Ok(buffer)
-}
-
-fn read_dib_header_data(file: &mut File) -> Result<[u8; 8], Error> {
-    let mut buffer = [0; 8];
-    file.seek(SeekFrom::Start(0x12))?;
-    file.by_ref().take(8).read(&mut buffer)?;
-    Ok(buffer)
-}
-
-fn read_bitmap_data(file: &mut File, header: &bmp::BitmapFileHeader) -> Result<Vec<u8>, Error> {
-    file.seek(SeekFrom::Start(header.offset as u64))?;
-    let mut buffer: Vec<u8> = Vec::new();
-    let bytes_read = file.read_to_end(&mut buffer)?;
-    println!("Read {:?} bytes", bytes_read);
-    Ok(buffer)
+    let bmp_file = bmp::read_bmp_file(&img_path).unwrap();
+    println!("{:?}", bmp_file);
 }
