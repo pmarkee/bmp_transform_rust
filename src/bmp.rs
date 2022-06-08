7f651fb78bbe8027884ebd6d9a184ae5a5ec7a32
Peter Marki <marpeter00@gmail.com>
2022-06-08 21:15:33 +0200

    Initial commit - read bmp file

---
+++ src/bmp.rs
@@ -0,0 +1,78 @@
+use byteorder::ByteOrder;
+use byteorder::LittleEndian;
+use std::iter::Iterator;
+
+// TODO rethink what should be public and private here
+#[derive(Debug)]
+pub struct Pixel {
+    pub red: u8,
+    pub green: u8,
+    pub blue: u8,
+}
+
+#[derive(Debug)]
+pub struct BitmapFileHeader {
+    pub signature: u16,
+    pub other_bytes: u64,
+    pub offset: u32,
+}
+
+#[derive(Debug)]
+pub struct BitmapDibHeader {
+    pub width: u32,
+    pub height: u32,
+}
+
+#[derive(Debug)]
+pub struct Bitmap {
+    pub width: u32,
+    pub height: u32,
+    pub pixels: Vec<Pixel>,
+}
+
+#[derive(Debug)]
+pub struct BmpFile {
+    pub bitmap_file_header: BitmapFileHeader,
+    pub bitmap: Bitmap,
+}
+
+pub fn read_bitmap_file_header(buffer: &[u8; 14]) -> BitmapFileHeader {
+    BitmapFileHeader {
+        signature: LittleEndian::read_u16(buffer),
+        other_bytes: LittleEndian::read_u64(&buffer[2..10]),
+        offset: LittleEndian::read_u32(&buffer[10..14]),
+    }
+}
+
+pub fn read_dib_header(buffer: &[u8; 8]) -> BitmapDibHeader {
+    BitmapDibHeader {
+        width: LittleEndian::read_u32(buffer),
+        height: LittleEndian::read_u32(&buffer[4..]),
+    }
+}
+
+pub fn read_bitmap(buffer: &Vec<u8>, dib_header: &BitmapDibHeader) -> Bitmap {
+    let padding_size = dib_header.width * 3 % 4;
+    let mut buffer_iterator = buffer.iter();
+
+    let mut pixels: Vec<Pixel> = Vec::new();
+
+    for _ in 0..dib_header.height {
+        for _ in 0..dib_header.width {
+            // TODO handle errors instead of unwrap
+            let blue = buffer_iterator.next().unwrap().clone();
+            let green = buffer_iterator.next().unwrap().clone();
+            let red = buffer_iterator.next().unwrap().clone();
+            pixels.push(Pixel { red, green, blue });
+        }
+        for _ in 0..padding_size {
+            buffer_iterator.next();
+        }
+    }
+
+    Bitmap {
+        width: dib_header.width,
+        height: dib_header.height,
+        pixels,
+    }
+}
